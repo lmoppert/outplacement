89c2f67e0de967a32f44fe21a88c5ba45e06b66e
Lutz Moppert <lutz@moppert.de>
2019-12-02 21:46:09 +0100

    Added navigation bar

--- par/views.py
+++ par/views.py
@@ -7,7 +7,7 @@ class IndexView(generic.ListView):
     template_name = 'par/index.html'
 
     def get_queryset(self):
-        return PAR.objects.all()[:10]
+        return PAR.objects.all().order_by('-number')[:10]
 
 
 class PARDetail(generic.DetailView):
